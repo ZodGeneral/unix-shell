c454509851e24a55489db99ddf979df38dd73f78
Austin Ray <adray1994@gmail.com>
2026-06-10 01:42:07 -0500

    Set up CMake project structure

---
+++ src/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+    std::cout << "Welcome to Austin's shell project.\n";
+
+    while (true)
+    {
+        std::cout << "$ ";
+        break;
+    }
+
+    return 0;
+}
